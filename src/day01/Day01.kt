d617451fe7c22640e90f17e7c815afc09743557c
BuildTools <unconfigured@null.spigotmc.org>
2021-12-01 19:09:57 +0100

    day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -9,8 +9,8 @@ fun main() {
 
 object Day01 : Day(1) {
 
-    override fun part1() = input.map(String::toInt).zipWithNext().count { it.first < it.second }
+    override fun part1() = input.map { it.toInt() }.zipWithNext().count { it.first < it.second }
 
-    override fun part2() = input.map(String::toInt).windowed(3)
+    override fun part2() = input.map { it.toInt() }.windowed(3)
         .map { it.sum() }.zipWithNext().count { it.first < it.second }
 }
